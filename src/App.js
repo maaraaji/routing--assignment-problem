7da07d9997d6a5571b56a3ef36a61460fb8fd26f
maaraa <maaraa@dotworld.in>
2018-02-22 12:46:17 +1100

    Add a simple navigation with two Links = One leading to "Users", One leading to "Courses"

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import React, { Component } from 'react';
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
 
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import './App.css';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
+        <nav>
+          <ul>
+            <Link to='/users' className="firstLink">Users</Link>
+            <Link to='/courses'>Courses</Link>
+          </ul>
+        </nav>
         <Route path='/users' exact component={Users}/>
         <Route path='/courses' exact component={Courses} />
       </div>
