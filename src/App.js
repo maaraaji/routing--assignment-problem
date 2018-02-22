cf48099bec839d9d69f79bbddedf5b90f0f61533
maaraa <maaraa@dotworld.in>
2018-02-22 14:42:22 +1100

    Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 
 import Courses from './containers/Courses/Courses';
+import Course from './containers/Course/Course';
 import Users from './containers/Users/Users';
 
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -16,8 +17,11 @@ class App extends Component {
             <Link to='/courses'>Courses</Link>
           </ul>
         </nav>
-        <Route path='/users' exact component={Users}/>
-        <Route path='/courses' exact component={Courses} />
+        <Switch>
+          <Route path='/users' exact component={Users}/>
+          <Route path='/courses' exact component={Courses} />
+          <Route path={'/:id'} exact component={Course}/>
+        </Switch>
       </div>
     );
   }
--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './Courses.css';
+import { Link } from 'react-router-dom';
 
 class Courses extends Component {
     state = {
@@ -18,7 +19,9 @@ class Courses extends Component {
                 <section className="Courses">
                     {
                         this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
+                            return <article className="Course" key={course.id}>
+                              <Link to={'/' + course.id}>{course.title}</Link>
+                            </article>;
                         } )
                     }
                 </section>
